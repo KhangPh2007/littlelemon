8983533b5f52acdd71c06c860f3c2a8921bbb8ad
Khang Phan <khangphan.ht@gmail.com>
2023-02-24 15:48:10 -0800

    made appbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import AppBar from './components/AppBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppBar/>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Homepage</a>
+          </li>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+        </ul>
+      </nav>
+      <a href="/book" role="button">
+        Book a table
+      </a>
     </div>
   );
 }
---
+++ src/components/AppBar.js
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { Button, Grid, useMediaQuery, Menu, MenuItem } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "./assets/logo.jpg";
+
+function AppBar() {
+  const logoStyle = {
+    maxHeight: 300,
+    maxWidth: 200,
+  };
+  
+  const isSmallScreen = useMediaQuery("(max-width: 800px)");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      xs={12}
+      sm={8}
+    >
+      <Grid item xs={2}>
+        {isSmallScreen ? (
+          <Button onClick={handleClick}>
+            <MenuIcon />
+          </Button>
+        ) : (
+          <Button>
+            <MenuIcon />
+          </Button>
+        )}
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>Home</MenuItem>
+          <MenuItem onClick={handleClose}>About</MenuItem>
+          <MenuItem onClick={handleClose}>Reservation</MenuItem>
+          <MenuItem onClick={handleClose}>Login</MenuItem>
+          <MenuItem onClick={handleClose}>Sign Up</MenuItem>
+        </Menu>
+      </Grid>
+      <Grid item xs={4}>
+        <img src={logo} alt="logo" style={logoStyle} />
+      </Grid>
+      {!isSmallScreen && (
+        <Grid
+          item
+          container
+          xs={6}
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <Button size="large">Home</Button>
+          <Button size="large">About</Button>
+          <Button size="large">Reservation</Button>
+          <Button size="large">Login</Button>
+          <Button size="large">Sign Up</Button>
+        </Grid>
+      )}
+    </Grid>
+  );
+}
+
+export default AppBar;
